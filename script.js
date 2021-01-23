db57b75f380b4cc9efb416702f8f2d68edd8fae7
HagaRashi <Haganehitoarasi@gmail.com>
2021-01-23 21:47:48 +0600

    MD imp7.7.7 fix6

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(()=>{
         window.setTimeout(()=>{
             $("body").load("second.html")
             window.setTimeout(()=>{
-                $(".answ").css("animation-name","none")
+                $(".answ").css({"animation-name": "none", "box-shadow":"0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)"})
             })
         },800)
     })
