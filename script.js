b3d44ace75228eddb6bc9211645a6322568dc3bf
HagaRashi <Haganehitoarasi@gmail.com>
2021-01-27 22:51:05 +0600

    AN imp5

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ $(()=>{
             $("body").load("second.html")
             window.setTimeout(()=>{
                 $(".answ").addClass("appeared")
+                $(".answ").on("click", ()=>{
+                    $(this).addClass("active")
+                })
             },500)
         },800)
     })
