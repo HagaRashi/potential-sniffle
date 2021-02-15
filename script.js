c746b8b16c25c1da9d05e1d90f36786c490be3fd
hagarashi <haganehitoarashi@gmail.com>
2021-02-15 22:23:07 +0600

    k

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 $(()=>{
-    $("#megaStart").on("click",()=>{
-        $("#megamain").addClass("disapearing")
-        $('#megaanim').on("animationend",()=>{
-            $("body").load("second.html")
-            $(".answ").on("animationend",()=>{
-                $(".answ").addClass("appeared")
-                $(".answ").on("click", ()=>{
-                    $(this).addClass("active")
-            })
-        },500)
-    })})
+    $("#megamain"),on("animationend",()=>{
+        console.log("xxx")
+    })
 })
