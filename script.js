6ab7101730ec7cf38ea3af8a30a531c33f9c9d0e
hagarashi <haganehitoarashi@gmail.com>
2021-02-17 14:58:33 +0600

    upd1

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
+function toggle(x,y){
+    $(x).one("click",function(){
+        console.log($(this))
+        $(x).off("click");
+        if(y){
+            $(this).addClass("selected");
+            toggle(this,false);
+        } else{
+            $(this).removeClass("selected");
+            toggle(".answ",true);
+        }
+    })
+}
 $(()=>{
     $("#start").one("click",()=>{
         $("#main").addClass("vanishing").removeClass("appearing");
         $("#main.vanishing").one("animationend",()=>{
-            $("body").load("second.html",()=>{
-                $(".answ").one("click",function(){
-                    console.log($(this))
-                    $( this ).addClass("selected");
-                    $(".answ").off("click");
-                })
-            });
+            $("body").load("second.html",toggle(".answ",true));
         })
     })
 })
