dfbf625ae0447307214f106cff675f929bfa72b5
hagarashi <haganehitoarashi@gmail.com>
2021-02-17 18:47:08 +0600

    k

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-function toggle(x,y){
+function toggle(x){
     console.log($(x).one("click",function(){
         $(x).removeClass("selected");
         $(this).addClass("selected")
+        toggle($(".answ"))
     }))
 }
 $(()=>{
     $("#start").one("click",()=>{
         $("#main").addClass("vanishing").removeClass("appearing");
         $("#main.vanishing").one("animationend",()=>{
-            $("body").load("second.html",()=>{toggle($(".answ"),true)});
+            $("body").load("second.html",()=>{toggle($(".answ"))});
         })
     })
 })
